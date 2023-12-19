c6aa42328d322dae942c02b795ef261b11d307c3
alissonrhuant3 <alissonrhuant3@gmail.com>
2023-12-19 20:46:49 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/alisson/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.alisson.workshopmongo.repository;
+
+import com.alisson.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/alisson/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.alisson.workshopmongo.services;
+
+import com.alisson.workshopmongo.domain.User;
+import com.alisson.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+       return repo.findAll();
+    }
+}
